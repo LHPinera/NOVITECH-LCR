5d9dc4abde2d4e422ff259dff4bf34e2ce48e7a1
Luis Pinera <luispinera@hotmail.com>
2023-01-06 11:16:00 -0600

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/DashBoard.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace RepMovilLeeChips
+{
+    partial class DashBoard
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbFecha = new System.Windows.Forms.TextBox();
+            this.tbAntena = new System.Windows.Forms.TextBox();
+            this.tbFolio = new System.Windows.Forms.TextBox();
+            this.btnDetener = new System.Windows.Forms.Button();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(350, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lectura de chip REPUVE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 278);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Antena";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 326);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(118, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Folio REPUVE:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(123, 232);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha";
+            // 
+            // tbFecha
+            // 
+            this.tbFecha.Location = new System.Drawing.Point(374, 222);
+            this.tbFecha.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbFecha.Name = "tbFecha";
+            this.tbFecha.Size = new System.Drawing.Size(205, 26);
+            this.tbFecha.TabIndex = 4;
+            // 
+            // tbAntena
+            // 
+            this.tbAntena.Location = new System.Drawing.Point(374, 268);
+            this.tbAntena.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbAntena.Name = "tbAntena";
+            this.tbAntena.Size = new System.Drawing.Size(49, 26);
+            this.tbAntena.TabIndex = 5;
+            // 
+            // tbFolio
+            // 
+            this.tbFolio.Location = new System.Drawing.Point(374, 315);
+            this.tbFolio.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbFolio.Name = "tbFolio";
+            this.tbFolio.Size = new System.Drawing.Size(148, 26);
+            this.tbFolio.TabIndex = 6;
+            // 
+            // btnDetener
+            // 
+            this.btnDetener.Location = new System.Drawing.Point(374, 534);
+            this.btnDetener.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnDetener.Name = "btnDetener";
+            this.btnDetener.Size = new System.Drawing.Size(112, 35);
+            this.btnDetener.TabIndex = 7;
+            this.btnDetener.Text = "Detener";
+            this.btnDetener.UseVisualStyleBackColor = true;
+            this.btnDetener.Click += new System.EventHandler(this.Detener);
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(201, 534);
+            this.btnIniciar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(112, 35);
+            this.btnIniciar.TabIndex = 8;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.Iniciar);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RepMovilLeeChips.Properties.Resources.enc_repuve;
+            this.pictureBox1.Location = new System.Drawing.Point(40, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(261, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(40, 151);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(640, 263);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ultimo chip leido";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Location = new System.Drawing.Point(374, 454);
+            this.tbTotal.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(148, 26);
+            this.tbTotal.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(123, 454);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Total de chips leidos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 625);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.btnDetener);
+            this.Controls.Add(this.tbFolio);
+            this.Controls.Add(this.tbAntena);
+            this.Controls.Add(this.tbFecha);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "NOVITECH - Lectura de chip REPUVE";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbFecha;
+        private System.Windows.Forms.TextBox tbAntena;
+        private System.Windows.Forms.TextBox tbFolio;
+        private System.Windows.Forms.Button btnDetener;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/DashBoard.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Impinj.OctaneSdk;
+
+namespace RepMovilLeeChips
+{
+    public partial class DashBoard : Form
+    {
+        static ImpinjReader reader = new ImpinjReader();
+        static int _chips;
+        static string SQLconstr = @"Data Source=CYBOLT-RPV-SRV\SQLEXPRESS;Initial Catalog=arcomovilrepuve;User Id=sa;Password=Admin123";
+        static string Arco;
+        static int ArcoId;
+        static string hostname;
+
+        public DashBoard()
+        {
+            InitializeComponent();
+            var appSettings = ConfigurationManager.AppSettings;
+            CheckForIllegalCrossThreadCalls = false;
+            _chips = 0;
+            Arco = appSettings["Arco"];
+            hostname = appSettings["IPLector"];
+            try
+            {
+                reader.Connect(hostname);
+                Settings settings = reader.QueryDefaultSettings();
+                settings.Report.IncludeAntennaPortNumber = true;
+                settings.ReaderMode = ReaderMode.AutoSetDenseReader;
+                settings.SearchMode = SearchMode.SingleTarget;
+                settings.Session = 2;
+                settings.Antennas.EnableAll();
+                settings.Antennas.TxPowerMax = true;
+                settings.Antennas.RxSensitivityMax = true;
+                //settings.Antennas.GetAntenna(1).IsEnabled = true;
+                //settings.Antennas.GetAntenna(1).MaxTxPower = true;
+                //settings.Antennas.GetAntenna(1).MaxRxSensitivity = true;
+                reader.ApplySettings(settings);
+                reader.TagsReported += OnTagsReported;
+
+                tbFecha.ReadOnly = true;
+                tbAntena.ReadOnly = true;
+                tbFolio.ReadOnly = true;
+                tbTotal.ReadOnly = true;
+
+                btnDetener.Enabled = true;
+                btnIniciar.Enabled = false;
+
+                reader.Start();
+
+
+            }
+            catch (OctaneSdkException e)
+            {
+                Console.WriteLine("Octane SDK excepción {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Excepción {0}", e.Message);
+            }
+        }
+
+
+        private void OnTagsReported(ImpinjReader sender, TagReport report)
+        {
+            Tag tag = report.Tags.Last();
+
+            Lectura lectura = new Lectura()
+            {
+                Fecha = DateTime.Now,
+                Antena = tag.AntennaPortNumber.ToString(),
+                Folio = ConvertHex(tag.Epc.ToHexString()),
+                Contador = ++_chips
+            };
+
+            ShowData(lectura);
+            GrabaChip(lectura);
+
+
+        }
+
+        private string ConvertHex(String hexString)
+        {
+            try
+            {
+                string ascii = string.Empty;
+
+                for (int i = 0; i < hexString.Length; i += 2)
+                {
+                    String hs = string.Empty;
+
+                    hs = hexString.Substring(i, 2);
+                    uint decval = System.Convert.ToUInt32(hs, 16);
+                    char character = System.Convert.ToChar(decval);
+                    ascii += character;
+
+                }
+
+                return ascii;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+
+            return string.Empty;
+        }
+
+        private void ShowData(Lectura lectura)
+        {
+            tbFecha.Text = lectura.Fecha.ToShortDateString() + " - "+lectura.Fecha.ToShortTimeString();
+            tbAntena.Text = lectura.Antena;
+            tbFolio.Text = lectura.Folio;
+            tbTotal.Text = lectura.Contador.ToString();
+        }
+
+        private void Detener(object sender, EventArgs e)
+        {
+            try
+            {
+                reader.Stop();
+                btnDetener.Enabled = false;
+                btnIniciar.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Excepcion {0}", ex.Message);
+            }
+        }
+
+        private void Iniciar(object sender, EventArgs e)
+        {
+            try
+            {
+                reader.Start();
+                btnIniciar.Enabled = false;
+                btnDetener.Enabled = true;
+                tbFecha.Text = "";
+                tbAntena.Text = "";
+                tbFolio.Text = "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Excepcion {0}", ex.Message);
+            }
+        }
+
+        private void GrabaChip(Lectura lectura)
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(SQLconstr);
+                conn.Open();
+                using (conn)
+                {
+                    Chip NChip = new Chip()
+                    {
+                        Chip_Arco = Arco,
+                        Chip_Arco_Id = ArcoId,
+                        Chip_Fecha = lectura.Fecha,
+                        Chip_Antena = lectura.Antena,
+                        Chip_Folio = lectura.Folio,
+                        Chip_Busqueda = 0,
+                        Chip_Econtrado = 0,
+                        Chip_Enviado = 0
+                    };
+                  
+                    try
+                    {
+                        SqlCommand cmdInserta = new SqlCommand("INSERT INTO Chips_Leidos (Chip_Arco, Chip_Arco_Id, Chip_Fecha, Chip_Antena, Chip_Folio, Chip_Busqueda, Chip_Econtrado, Chip_Enviado) VALUES(@par1, @par2, @par3, @par4, @par5,  @par6, @par7, @par8)", conn);
+                        cmdInserta.Parameters.AddWithValue("@par1", NChip.Chip_Arco);
+                        cmdInserta.Parameters.AddWithValue("@par2", NChip.Chip_Arco_Id);
+                        cmdInserta.Parameters.AddWithValue("@par3", NChip.Chip_Fecha);
+                        cmdInserta.Parameters.AddWithValue("@par4", NChip.Chip_Antena);
+                        cmdInserta.Parameters.AddWithValue("@par5", NChip.Chip_Folio);
+                        cmdInserta.Parameters.AddWithValue("@par6", NChip.Chip_Busqueda);
+                        cmdInserta.Parameters.AddWithValue("@par7", NChip.Chip_Econtrado);
+                        cmdInserta.Parameters.AddWithValue("@par8", NChip.Chip_Enviado);
+                        cmdInserta.ExecuteNonQuery();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write(ex);
+                    }
+                }
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex);
+                MessageBox.Show("No se encuentra el servidor de datos", "Error del red");
+                Application.Exit();
+            }
+        }
+    }
+    public class Lectura
+    {
+        public DateTime Fecha { get; set; }
+        public string Antena { get; set; }
+        public string Folio { get; set; }
+        public int Contador { get; set; }
+    }
+
+    public class Chip
+    {
+        public string Chip_Arco { get; set; }
+        public int Chip_Arco_Id { get; set; }
+        public DateTime Chip_Fecha { get; set; }
+        public string Chip_Antena { get; set; }
+        public string Chip_Folio { get; set; }
+        public int Chip_Busqueda { get; set; }
+        public int Chip_Econtrado { get; set; }
+        public int Chip_Enviado { get; set; }
+
+    }
+}
